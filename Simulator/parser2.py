22b6f2d85b8e109b943fb599a2771eac245ea529
Stephanie <stephanie.spielman@gmail.com>
2014-02-04 15:28:42 -0600

    Remove ineffective parsers and add in effective one

--- Simulator/parser2.py
+++ Simulator/parser2.py
@@ -1,76 +1,107 @@
+import re
+
 class Node:
-	'''Node class. Each tree itself is a node containing subnodes all the way through.'''
-	def __init__(self, name, lchild, rchild, BL):
-		self.name = name # this can either be None (internal) or a leaf name.
-		self.lchid = lchild # Left-hand child (node). None if leaf
-		self.rchild = rchild # right-hand child (node). None if leaf.
-		self.BL = BL # Branch length leading up to node
+	def __init__(self):
+		self.name = None # this can either be None (internal) or a leaf name.
+		self.children = [] # list of children, each of which is a node
+		self.BL = None # Branch length leading up to node
+
+
+def printTree(tree, level=0):
+	indent=''
+	for i in range(level):
+		indent+='\t'
+	print indent, "Name:", tree.name, "BL:", tree.BL
+	if len(tree.children)>0:
+		#print indent, "Children:"
+		for node in tree.children:
+			printTree(node, level+1)
+
+def readBranchLength(tstring, index):
+	print index
+	assert(tstring[index]==':')
+	
+	end = index
+	while True:
+		end += 1
+		if end==len(tstring):
+			break
+		if tstring[end]==',' or tstring[end]==')':
+			break
+	BL = float( tstring[index+1:end] )
+	return BL, end
+
+def readLeaf(tstring, index):
+	end = index
+	node = Node()
+	while True:
+		end += 1
+		assert( end<len(tstring) )
+		
+		# No branch lengths
+		if tstring[end]==',' or tstring[end]==')':
+			node.name = tstring[index+1:end]
+			print "first", node.name
+			node.BL = None
+			break
+		
+		# Given branch lengths	
+		if tstring[end]==':' :
+			node.name = tstring[index:end]
+			#print "second", node.name
+			node.BL, end = readBranchLength(tstring, end)
+			break
+	return node, end
+
+
+
+def parseTree(tstring, index=0):
+
+	assert(tstring[index]=='(')
+
+	index += 1
+	node = Node()
+	while True:
+	
+		if tstring[index]=='(':
+			subtree, index=parseTree(tstring, index)
+			node.children.append( subtree )
+		
+		elif tstring[index]==',':
+			index += 1
+			
+		elif tstring[index]==')':
+			index+=1
+			if index<len(tstring):
+				BL, index = readBranchLength(tstring, index)
+				node.BL = BL
+			break
+		
+		else:
+			subtree, index = readLeaf(tstring, index)
+			node.children.append( subtree )
+	
+	return node, index
 
 
 def readTree(treefile):
-	''' Takes a file containing a fully bifurcating Newick tree and parses. Branch lengths required at every node.'''
+
 	t = open(treefile, 'r')
 	tstring = t.read()
 	t.close()
 	
 	############ Format tree appropriately. ############### 
 	tstring = re.sub(r"\s", "", tstring)                  # Remove all whitespace
-	tstring = re.sub("\d\.*\d*e-\d+", "0.00001", tstring) # If small enough to be exponent, this is a fine approx
-	tstring = re.sub(r"\:\.", ":0.", tstring)             # Be sure any branch length that is a decimal has at least one digit before decimal (as in, 0.9 instead of .9)
 	tstring = re.sub(r"\)[^:]", "):0", tstring)           # Be sure all nodes have a branch length
 	tstring = tstring.rstrip(';')                         # Remove trailing semi-colon
-	
+
 	# Go to town
-	tree = parseTree(tstring)
+	(tree, index) = parseTree(tstring,  0)
 	return tree
 
+tree = readTree('small.tre')
+printTree(tree)
 
-
-
-
-
-def parseTree(tstring):
-	''' Does the bulk of the tree parsing'''
-	
-	stopChar = re.compile('[,\)]') # 
-	
-	## If we have an open parenthesis, there is a subtree to be read
-	if tstring[0]=='(':
-		(left_string, right_string) = subParse(tstring)
-		BL = grabBL(tstring)
-		return( Node(None, parseTree(left_string), parseTree(right_string), BL) ) ## I'm missing a step of the logic here, right???
-	
-	# If no subtree, we've arrived at a leaf
-	else:
-		name, BL = tstring.split( ":" )
-		return( Node(name, None, None, BL) )
-	
-	
-	
-	
-def subParse(tstring):
-	tstring = tstring.lstrip('(') # Remove leading parenthesis
-	
-	#March down the tstring. When we find (==) character is a comma, we've reached a split
-	paren_count=0
-	for index in len(tstring):
-		if tstring[index]=='(':	paren_count+=1
-		if tstring[index]==')':	paren_count-=1
-		if parent_count==0 and tstring[index]==',':
-			left_string = tstring[ 0 : index ]
-			right_string = tstring[ (index+1) : ] # be sure not to include the comma
-	return(left_string, right_string)	
-	
-
-def grabBL(tstring):
-	
-	stopChar = re.compile(r'?P<branchlen>(:\d+\.*\d*)[,\)]')
-	findLength = stopChar.search(tstring)
-	if findLength.group("branchlen"):	BL = float(findLength.group("branchlen"))
-	
-	return BL
-	
-	
 	
 	
 	
