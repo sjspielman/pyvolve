e5995548fc1b82490f9103d60dd83d8f1269810d
Stephanie <stephanie.spielman@gmail.com>
2014-02-04 17:39:53 -0600

    Removed old dendropy file

--- Simulator/examineDP.py
+++
@@ -1,8 +0,0 @@
-## Figure out how to use dendropy using the rcoal(10) in small.tre. 2/2/14.
-
-from ete2 import Tree
-
-t1 = Tree("small.tre")
-
-for node in t1.traverse("preorder"):
-	print node.name
