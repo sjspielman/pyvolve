02c8cb1d5cfeef456ac4339845b029211e7c5e07
Stephanie <stephanie.spielman@gmail.com>
2014-02-05 18:29:44 -0600

    Created modelBuilder.py to calculate the SellaHirsh instantaneous rate matrix

---
+++ Simulator/modelBuilder.py
@@ -0,0 +1,259 @@
+import numpy as np
+from Bio.Seq import Seq
+from Bio.Alphabet import *
+import re
+
+class Model(object):
+	def __init__(self, mu, kappa, aminoFreqs):
+		
+		# Need to be provided by user
+		self._MU = mu
+		self._KAPPA = kappa
+		self._STATE = aminoFreqs
+		
+		# Unless we have a dramatic and unfortunate shift in the genetic code....
+		self._PYR     = ['C', 'T']
+		self._PUR     = ['A', 'G']
+		self._AA_CODE = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+		self._GENCODE = [['GCA', 'GCC', 'GCG', 'GCT'], ['TGC','TGT'], ['GAC', 'GAT'], ['GAA', 'GAG'], ['TTC', 'TTT'], ['GGA', 'GGC', 'GGG', 'GGT'], ['CAC', 'CAT'], ['ATA', 'ATC', 'ATT'], ['AAA', 'AAG'], ['CTA', 'CTC', 'CTG', 'CTT', 'TTA', 'TTG'], ['ATG'], ['AAC', 'AAT'], ['CCA', 'CCC', 'CCG', 'CCT'], ['CAA', 'CAG'], ['AGA', 'AGG', 'CGA', 'CGC', 'CGG', 'CGT'] , ['AGC', 'AGT', 'TCA', 'TCC', 'TCG', 'TCT'], ['ACA', 'ACC', 'ACG', 'ACT'], ['GTA', 'GTC', 'GTG', 'GTT'], ['TGG'], ['TAC', 'TAT']]
+		self._CODONS  = {"AAA":"K", "AAC":"N", "AAG":"K", "AAT":"N", "ACA":"T", "ACC":"T", "ACG":"T", "ACT":"T", "AGA":"R", "AGC":"S", "AGG":"R", "AGT":"S", "ATA":"I", "ATC":"I", "ATG":"M", "ATT":"I", "CAA":"Q", "CAC":"H", "CAG":"Q", "CAT":"H", "CCA":"P", "CCC":"P", "CCG":"P", "CCT":"P", "CGA":"R", "CGC":"R", "CGG":"R", "CGT":"R", "CTA":"L", "CTC":"L", "CTG":"L", "CTT":"L", "GAA":"E", "GAC":"D", "GAG":"E", "GAT":"D", "GCA":"A", "GCC":"A", "GCG":"A", "GCT":"A", "GGA":"G", "GGC":"G", "GGG":"G", "GGT":"G", "GTA":"V", "GTC":"V", "GTG":"V", "GTT":"V", "TAC":"Y", "TAT":"Y", "TCA":"S", "TCC":"S", "TCG":"S", "TCT":"S", "TGC":"C", "TGG":"W", "TGT":"C", "TTA":"L", "TTC":"F", "TTG":"L", "TTT":"F"}
+		
+	
+	def isSyn(self, source, target):
+		''' Given a source codon and target codon, return True if the change is nonsynonymous.'''
+		if (self._CODONS[source] == self._CODONS[target]):
+			return True
+		else:
+			return False
+	
+	def getCodonFreq(self, codon):
+		''' Get the frequency for a given codon. This function assumes that all synonymous codons have the same frequency. '''
+		aa = self._CODONS[codon]
+		Freq = self._STATE[self._AA_CODE.index(aa)]
+		return Freq	
+	
+	def isTI(self, source, target):
+		''' Returns True for transition, False for transversion.'''
+		if (source in self._PYR and target in self._PUR):
+			return False
+		else:
+			return True
+	
+	def calcMutProb(self, source, target):
+		''' Calculates a substitution probability between two codons. If single mutation, return the probability/rate. Else, return 0. ''' 
+		mydiff=''
+		count=0
+		for i in range(3):
+			if source[i] == target[i]:	
+				continue
+			else:	
+				mydiff+=source[i]
+				mydiff+=target[i]
+		
+		# Either no change or >1 mutational steps. It's probability is zero.	
+		if len(mydiff)!=2:
+			return 0
+		
+		# If a single mutational step away, return the probability
+		else:		
+			if self.isTI(mydiff[0], mydiff[1]):
+				
+				if self.isSyn(source, target):
+					return self.synTI(source, target)
+				else:
+					return self.nonSynTI(source, target)
+			else:
+				
+				if self.isSyn(source, target):
+					return self.synTV(source, target)
+				else:
+					return self.nonSynTV(source, target)
+				
+				
+				
+					
+	def buildTransitionMatrix(self):
+		return 0			
+
+	########################################################## 
+	## Base functions for computing rates. Not implemented. ## 
+	def synTI(self, source, target):
+		return 0
+	def synTV(self, source, target):
+		return 0
+	def nonSynTI(self, source, target):
+		return 0
+	def nonSynTV(self, source, target):
+		return 0
+	###########################################################
+		
+	
+		
+class SellaModel(Model):
+	def __init__(self, *args, **kwargs):
+		super(SellaModel, self).__init__(*args, **kwargs)
+	
+	
+	##################### Functions for calculating probabilities of the different substitution options. ###########################
+	def fix(self, source_freq, target_freq):
+		''' Given pi(i) and pi(j), where pi() is the equilibrium a given codon in that column, return probability_of_fixation_(i->j). '''
+		if source_freq == target_freq:
+			return 1
+		else:
+			return ( (np.log(target_freq) - np.log(source_freq)) / (1 - source_freq/target_freq) )
+
+
+	def synTI(self, source, target):
+		''' Probability of synonymous transition '''
+		return ( self._MU * self._KAPPA )
+	
+	
+	def synTV(self, source, target):
+		''' Probability of synonymous tranversion '''
+		return ( self._MU )
+	
+	
+	def nonSynTI(self, source, target):
+		''' Probability of nonsynonymous transition '''
+		sFreq = self.getCodonFreq(source)
+		tFreq = self.getCodonFreq(target)
+		return ( self._MU * self._KAPPA * self.fix(sFreq, tFreq) )				
+	
+		
+	def nonSynTV(self, source, target):
+		''' Probability of nonsynonymous tranversion '''
+		sFreq = self.getCodonFreq(source)
+		tFreq = self.getCodonFreq(target)
+		return ( self._MU * self.fix(sFreq, tFreq) )	
+	##################################################################################################################################
+
+
+	def buildTransitionMatrix(self):
+		''' Builds the matrix Q '''
+		
+		transMatrix = np.empty([61,61]) # Look at me, hardcoding that there are 61 codons!
+		source_count=0
+		
+		for source in self._CODONS:
+			target_count=0
+			for target in self._CODONS:
+				rate = self.calcMutProb(source, target)
+				assert (rate>=0 and rate<1), "Fixation probability is exceedingly incorrect."
+				transMatrix[source_count][target_count] = rate
+				target_count+=1
+				
+			# Fill in the diagonal position so the row sums to 0
+			transMatrix[source_count][source_count]= -1*(np.sum( transMatrix[source_count] ))
+			assert (np.sum(transMatrix[source_count]==0)), "Row in matrix does not sum to 0."
+			source_count+=1
+		
+		return transMatrix
+			
+			
+	
+	
+	
+	
+	
+
+codonFreqs = [0.006035686218971376, 0.006035686218971376, 0.006035686218971376, 0.006035686218971376, 0.04041378723808315, 0.04041378723808315, 0.032098342321156166, 0.032098342321156166, 0.043116404375596995, 0.043116404375596995, 0.009440547545140699, 0.009440547545140699, 0.011632846243747412, 0.011632846243747412, 0.011632846243747412, 0.011632846243747412, 0.04573777513398951, 0.04573777513398951, 0.024498759238302317, 0.024498759238302317, 0.024498759238302317, 0.009148227493706296, 0.009148227493706296, 0.013470358817488972, 0.013470358817488972, 0.013470358817488972, 0.013470358817488972, 0.013470358817488972, 0.013470358817488972, 0.004875895626235194, 0.01762993572322193, 0.01762993572322193, 0.004788541107242323, 0.004788541107242323, 0.004788541107242323, 0.004788541107242323, 0.04826387436585986, 0.04826387436585986, 0.0036601640920194563, 0.0036601640920194563, 0.0036601640920194563, 0.0036601640920194563, 0.0036601640920194563, 0.0036601640920194563, 0.0032299254578469906, 0.0032299254578469906, 0.0032299254578469906, 0.0032299254578469906, 0.0032299254578469906, 0.0032299254578469906, 0.022423097043176427, 0.022423097043176427, 0.022423097043176427, 0.022423097043176427, 0.01446788129594178, 0.01446788129594178, 0.01446788129594178, 0.01446788129594178, 0.016631129371868093, 0.02687200552651542, 0.02687200552651542]
+myModel=SellaModel(1e-2, 4.5, codonFreqs)
+myModel.buildTransitionMatrix()
+			
+
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		
+	
+	
+	
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	
+	
--- Simulator/simFxns.py
+++ Simulator/simFxns.py
@@ -3,21 +3,23 @@ from Bio.Seq import Seq
 from Bio.Alphabet import *
 import re
 import random as rn
+import numpy as np
 
-##### Day 1 of Stephanie Writes a Simulator #####
-#################################################
 
 #Alphabetical list of sense codons
 codons=["AAA", "AAC", "AAG", "AAT", "ACA", "ACC", "ACG", "ACT", "AGA", "AGC", "AGG", "AGT", "ATA", "ATC", "ATG", "ATT", "CAA", "CAC", "CAG", "CAT", "CCA", "CCC", "CCG", "CCT", "CGA", "CGC", "CGG", "CGT", "CTA", "CTC", "CTG", "CTT", "GAA", "GAC", "GAG", "GAT", "GCA", "GCC", "GCG", "GCT", "GGA", "GGC", "GGG", "GGT", "GTA", "GTC", "GTG", "GTT", "TAC", "TAT", "TCA", "TCC", "TCG", "TCT", "TGC", "TGG", "TGT", "TTA", "TTC", "TTG", "TTT"]
-
+#
 # Alphabetical genetic code
 gencode = [['GCA', 'GCC', 'GCG', 'GCT'], ['TGC','TGT'], ['GAC', 'GAT'], ['GAA', 'GAG'], ['TTC', 'TTT'], ['GGA', 'GGC', 'GGG', 'GGT'], ['CAC', 'CAT'], ['ATA', 'ATC', 'ATT'], ['AAA', 'AAG'], ['CTA', 'CTC', 'CTG', 'CTT', 'TTA', 'TTG'], ['ATG'], ['AAC', 'AAT'], ['CCA', 'CCC', 'CCG', 'CCT'], ['CAA', 'CAG'], ['AGA', 'AGG', 'CGA', 'CGC', 'CGG', 'CGT'] , ['AGC', 'AGT', 'TCA', 'TCC', 'TCG', 'TCT'], ['ACA', 'ACC', 'ACG', 'ACT'], ['GTA', 'GTC', 'GTG', 'GTT'], ['TGG'], ['TAC', 'TAT']]
 
 amino_acids=['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N' 'P', 'Q', 'R' , 'S', 'T', 'V', 'W', 'Y']
 
+
+slist=[['AAG'], ['AAT'], ['AAA'], ['AAC'], ['ACC', 'ACT', 'ACG'], ['ACA', 'ACT', 'ACG'], ['ACA', 'ACC', 'ACT'], ['ACA', 'ACC', 'ACG'], ['CGA', 'AGG'], ['AGT'], ['CGG', 'AGA'], ['AGC'], ['ATC', 'ATT'], ['ATA', 'ATT'], [], ['ATA', 'ATC'], ['CAG'], ['CAT'], ['CAA'], ['CAC'], ['CCC', 'CCT', 'CCG'], ['CCA', 'CCT', 'CCG'], ['CCA', 'CCC', 'CCT'], ['CCA', 'CCC', 'CCG'], ['AGA', 'CGC', 'CGT', 'CGG'], ['CGA', 'CGT', 'CGG'], ['AGG', 'CGA', 'CGC', 'CGT'], ['CGA', 'CGC', 'CGG'], ['TTA', 'CTC', 'CTT', 'CTG'], ['CTA', 'CTT', 'CTG'], ['TTG', 'CTA', 'CTC', 'CTT'], ['CTA', 'CTC', 'CTG'], ['GAG'], ['GAT'], ['GAA'], ['GAC'], ['GCC', 'GCT', 'GCG'], ['GCA', 'GCT', 'GCG'], ['GCA', 'GCC', 'GCT'], ['GCA', 'GCC', 'GCG'], ['GGC', 'GGT', 'GGG'], ['GGA', 'GGT', 'GGG'], ['GGA', 'GGC', 'GGT'], ['GGA', 'GGC', 'GGG'], ['GTC', 'GTT', 'GTG'], ['GTA', 'GTT', 'GTG'], ['GTA', 'GTC', 'GTT'], ['GTA', 'GTC', 'GTG'], ['TAT'], ['TAC'], ['TCC', 'TCT', 'TCG'], ['TCA', 'TCT', 'TCG'], ['TCA', 'TCC', 'TCT'], ['TCA', 'TCC', 'TCG'], ['TGT'], [], ['TGC'], ['CTA', 'TTG'], ['TTT'], ['CTG', 'TTA'], ['TTC']]
+
 def generateStateFreqs():
-	''' List of randomly-generated reasonable amino acid frequencies.'''
-	stateFreqs=[]
+	''' List of randomly-generated reasonable codon frequencies.'''
+	aaFreqs=[]
 	freq=float(1)
 	max=0.1
 	sum=float(0)
@@ -26,18 +28,25 @@ def generateStateFreqs():
 		while ((freq!=0) & (sum + freq > 1)):
 			freq = rn.uniform(0,max)
 		sum += freq
-		stateFreqs.append(freq)
-	stateFreqs.append(1-sum)
-	return stateFreqs
-
-# If we want to calculate lambda from the stateFreqs
-#def assignFitness(stateFreqs):
-# return 0
-
-def generateRoot(stateFreqs, gencode):
-	''' Generate a root codon sequence given equilibrium frequencies.'''
-	''' Given this root amino acid, select one of its codons at random.''' 
+		aaFreqs.append(freq)
+	aaFreqs.append(1-sum)
+	print aaFreqs
+	# Now we need to convert this to codons by dividing based on the number of synonymous codons that amino acid has
+	stateFreqs=[]
+	for i in range(20):		
+		num = len(gencode[i])
+		new = aaFreqs[i] / num
+		for j in range(num):
+			stateFreqs.append(new)
+		print new	
+	#print stateFreqs
 	
+	
+	
+
+def generateCodon(freqList, gencode):
+	''' Generate a codon sequence given a set of probabilities. For generating a root sequence, these are equilibrium state frequencies. For an evolving sequence, these are the transition probabilities calc'd for that branch.'''
+	''' Given this root amino acid, select one of its codons at random as all codons for a given amino acid all currently have the same frequencies (synonymous changes are therefore neutral).''' 	
 	r = rn.uniform(0,1)
 	i=0
 	sum=stateFreqs[i]
@@ -45,40 +54,14 @@ def generateRoot(stateFreqs, gencode):
 		i+=1
 		sum+=stateFreqs[i]	
 	codNum = rn.randint(0,len(gencode[i])-1)
+	
 	return gencode[i][codNum]
 	
-
-stateFreqs=generateStateFreqs()
-print stateFreqs
-rootSeq = generateRoot(stateFreqs, gencode)
-print rootSeq
+generateStateFreqs()
 
 
 
-def sim_subtree(baseSeq, node):
-	newSeq = evolve_branch(baseSeq, node) # Provide starting sequence, branch length along which to evolve
-	
-# Traverse and simulate along tree.	
-def traverseTree(baseSeq, tree):
-	
-	evolve_branch(baseSeq, tree)
 
-	if len(tree.children)>0:
-		print len(tree.childre)
-		for node in tree.children:
-			newSeq = evolve_branch(baseSeq, node)
-	
-	
-	
-	
-	
-	
-	print indent, tree.name, tree.BL, num
-	num+=1
-	if len(tree.children)>0:
-		for node in tree.children:
-			num=printTree(node, level+1, num)
-	return num
 
 
 
