80856f60793900b4d62562cc6a6f4775f6ac738d
Stephanie Spielman <stephaniespielman@macmini.local>
2014-02-10 11:20:31 -0600

    main edits

--- Simulator/main.py
+++ Simulator/main.py
@@ -5,103 +5,32 @@ import sys
 sys.path.append("src/")
 
 import misc
-from treeReader import Newick
-import modelBuilder
+import getState
+import newick
+from modelBuilder import Modeler
 from evolver import Evolver
 
-
-
-################################################################################
-def generateStateFreqs(aaFreqs=None):
-	''' Numpy array of randomly-generated reasonable codon frequencies, in alphabetical order.'''
-	
-	equal=True
-	if equal:
-		stateFreqs=np.zeros(61)
-		for i in range(len(stateFreqs)):
-			stateFreqs[i] = 1./61.
-		return stateFreqs
-	else:
-		fix_to_zero=[]
-		# no amino acid frequencies provided. generate random ones first
-		if aaFreqs==None:
-			aaFreqs = np.empty(20)
-			freq=float(1)
-			max=0.1
-			sum=float(0)
-			for i in range(19):
-				if i in fix_to_zero:
-					aaFreqs[i]=0
-					continue	
-				freq = rn.uniform(0,max)
-				while ((freq!=0) & (sum + freq > 1)):
-					freq = rn.uniform(0,max)
-				sum += freq
-				aaFreqs[i] = freq
-			aaFreqs[19] = (1.-sum)
-		stateFreqs = np.empty(61)
-		for c in range(61):
-			for a in range(len(molecules.genetic_code)):
-				numsyn = float(len(molecules.genetic_code[a]))
-				# We've found the correct amino acid
-				if molecules.codons[c] in molecules.genetic_code[a]:
-					if aaFreqs[a] == 0:
-						print c
-					stateFreqs[c] = (aaFreqs[a]/numsyn)
-					continue
-		return stateFreqs
-
-def readStateFreqs(alnfile, alnformat="fasta"):
-	''' Read in codon state frequencies from an alignment file '''
-	
-	bigSeq = ''
-	aln = AlignIO.read(alnfile, alnformat)
-	for entry in aln:
-		bigSeq += str(entry.seq)
-	# Remove ambig
-	bigSeq = bigSeq.translate(None, '-?N') #remove all gaps and ambiguous
-	
-	stateFreqs = np.zeros(61)
-	for i in range(0, len(bigSeq),3):
-		codon = bigSeq[i:i+3]
-		ind = molecules.codons.index(codon)
-		stateFreqs[ind]+=1
-	stateFreqs = np.divide(stateFreqs, len(bigSeq)/3)
-	
-	return stateFreqs
-
-################################################################################
-################################################################################
-def printTree(tree, level=0):
-	indent=''
-	for i in range(level):
-		indent+='\t'
-	print indent, tree.name, tree.branch, tree.seq
-	if len(tree.children)>0:
-		for node in tree.children:
-			print tree.seq
-			printTree(node, level+1)	
-###################################################################################
-	
 # get dna/amino/codon lists
 molecules = misc.Genetics()
 
-#stateFreqs=readStateFreqs('aln.phy', 'phylip-relaxed')
-
+# state frequencies
+#state = getState.
 
 # read in tree
-my_tree  = Newick("trees/10.tre")
-my_tree  = my_tree.readTree() # set True to print out the tree
+my_tree  = newick.readTree(file="trees/10.tre", show=False) # set True to print out the tree
 
 # Build model
+f = getState.EqualFreqs(type='codon', save='stateFreqs.txt')
+stateFreqs = f.setFreqs()
+np.savetxt('stateFreqs.txt', stateFreqs)
+
+
 mu=1e-4
 kappa=5.3
 omega=8
-stateFreqs = generateStateFreqs()
-np.savetxt('stateFreqs.txt', stateFreqs)
-myModel = modelBuilder.GY94Model(stateFreqs, kappa, omega)
+myModel = GY94Model(stateFreqs, kappa, omega)
 Q = myModel.buildQ()
-
+assert 1==0
 # Evolve along the tree
 outfile="stephanie.fasta"
 sequenceLength=3 # number of codons
