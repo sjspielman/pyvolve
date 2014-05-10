186964a9e36c19c59c99bd90cdb591c4bd914ede
Stephanie J Spielman <stephanie.spielman@gmail.com>
2014-05-10 15:56:52 -0500

    beginning testing for UserFreqs

--- Simulator/test/test_stateFreqs.py
+++ Simulator/test/test_stateFreqs.py
@@ -7,6 +7,9 @@ sys.path.append(SRC_CODE)
 from misc import Genetics
 from stateFreqs import *
 
+
+
+
 class stateFreqs_RandFreqs_Tests(unittest.TestCase):
 	''' Set of unittests for the EqualFreqs subclass of StateFreqs. Note that since random, cannot test exact values.'''
 	
@@ -92,6 +95,13 @@ class stateFreqs_RandFreqs_Tests(unittest.TestCase):
 		self.assertEqual(len(freqs), correct_len, msg= "RandFreqs has incorrect size for by=nuc, type=nuc.")
 
 
+
+
+
+
+
+
+
 class stateFreqs_EqualFreqs_Tests(unittest.TestCase):
 	''' Set of unittests for the EqualFreqs subclass of StateFreqs.'''
 	
@@ -170,11 +180,68 @@ class stateFreqs_EqualFreqs_Tests(unittest.TestCase):
 		freqs = self.eqFreqs.calcFreqs()
 		np.testing.assert_array_almost_equal(correct, freqs, decimal = self.dec, err_msg = "EqualFreqs not calculated properly for by=amino, type=nuc.")
 
+
+
+
+
+
+class stateFreqs_UserFreqs_Tests(unittest.TestCase):
+	''' Set of unittests for the UserFreqs subclass of StateFreqs.'''
+	
+	def setUp(self):
+		self.dec = 8 # For accuracy
+	
+	########### incorrect dictionaries provided. should raise assertions. ##############
+	def test_UserFreqs_calcFreqs_bycodon_badkey_length(self):
+		self.uFreqs = UserFreqs(by = 'codon', type = 'codon', freqs = {'A':0.5, 'T':0.5})
+		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
+	def test_UserFreqs_calcFreqs_bycodon_badkey_letters(self):
+		self.uFreqs = UserFreqs(by = 'codon', type = 'codon', freqs = {'ABC':1.0})
+		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
+	def test_UserFreqs_calcFreqs_bycodon_badkey_numbers(self):
+		self.uFreqs = UserFreqs(by = 'codon', type = 'codon', freqs = {123:1.0})
+		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
+	def test_UserFreqs_calcFreqs_bycodon_badvalues_toobig(self):
+		self.uFreqs = UserFreqs(by = 'codon', type = 'codon', freqs = {'AAA':1.1})
+		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
+	def test_UserFreqs_calcFreqs_bycodon_badvalues_zero(self):
+		self.uFreqs = UserFreqs(by = 'codon', type = 'codon', freqs = {'AAA':0})
+		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
+	def test_UserFreqs_calcFreqs_bycodon_badvalues_negative(self):
+		self.uFreqs = UserFreqs(by = 'codon', type = 'codon', freqs = {'AAA':-1})
+		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
+	def test_UserFreqs_calcFreqs_bycodon_badvalues_toosmall(self):
+		self.uFreqs = UserFreqs(by = 'codon', type = 'codon', freqs = {'AAA':0.5})
+		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
+	
+	########################### guess the by= from the keys ############################
+	#def test_UserFreqs_calcFreqs_bycodon_noby_badtriplet(self):
+	#	self.uFreqs = UserFreqs(type = 'codon', freqs = {'WWW':1.0})
+	#	self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
+
+
+
+
+
     
 if __name__ == '__main__':
 	run_tests = unittest.TextTestRunner()
-	test_suite_Equal = unittest.TestLoader().loadTestsFromTestCase(stateFreqs_EqualFreqs_Tests)
-	run_tests.run(test_suite_Equal)
-	test_suite_Rand = unittest.TestLoader().loadTestsFromTestCase(stateFreqs_RandFreqs_Tests)
-	run_tests.run(test_suite_Rand)
+	#test_suite_Equal = unittest.TestLoader().loadTestsFromTestCase(stateFreqs_EqualFreqs_Tests)
+	#run_tests.run(test_suite_Equal)
+	#test_suite_Rand = unittest.TestLoader().loadTestsFromTestCase(stateFreqs_RandFreqs_Tests)
+	#run_tests.run(test_suite_Rand)
+	test_suite_User = unittest.TestLoader().loadTestsFromTestCase(stateFreqs_UserFreqs_Tests)
+	run_tests.run(test_suite_User)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
