52824b0cc58c786be26bb97f3d3d21bb02795984
Stephanie J Spielman <stephanie.spielman@gmail.com>
2014-05-11 12:14:46 -0500

    moved to new file the tests intended to raise assertions

--- Simulator/test/test_stateFreqs.py
+++ Simulator/test/test_stateFreqs.py
@@ -15,17 +15,6 @@ class stateFreqs_RandFreqs_Tests(unittest.TestCase):
 	
 	def setUp(self):
 		self.dec = 8
-
-	############### type is not provided - should raise assertion #################
-	def test_RandFreqs_calcFreqs_bycodon_notype(self):
-		self.rFreqs = RandFreqs(by = 'codon')
-		self.assertRaises(AssertionError, self.rFreqs.calcFreqs)
-	def test_RandFreqs_calcFreqs_byamino_notype(self):
-		self.rFreqs = RandFreqs(by = 'amino')
-		self.assertRaises(AssertionError, self.rFreqs.calcFreqs)
-	def test_RandFreqs_calcFreqs_bynuc_notype(self):
-		self.rFreqs = RandFreqs(by = 'nuc')
-		self.assertRaises(AssertionError, self.rFreqs.calcFreqs)
 	
 	############################# by=codon tests ##################################
 	def test_RandFreqs_calcFreqs_bycodon_typecodon(self):
@@ -97,31 +86,13 @@ class stateFreqs_RandFreqs_Tests(unittest.TestCase):
 
 
 
-
-
-
-
-
 class stateFreqs_EqualFreqs_Tests(unittest.TestCase):
 	''' Set of "unittests" for the EqualFreqs subclass of StateFreqs.'''
 	
 	def setUp(self):
 		self.dec = 8 # For accuracy
 	
-	############### type is not provided - should raise assertion #################
-	def test_EqualFreqs_calcFreqs_bycodon_notype(self):
-		self.eqFreqs = EqualFreqs(by = 'codon')
-		self.assertRaises(AssertionError, self.eqFreqs.calcFreqs)
-	def test_EqualFreqs_calcFreqs_byamino_notype(self):
-		self.eqFreqs = EqualFreqs(by = 'amino')
-		self.assertRaises(AssertionError, self.eqFreqs.calcFreqs)
-	def test_EqualFreqs_calcFreqs_bynuc_notype(self):
-		self.eqFreqs = EqualFreqs(by = 'nuc')
-		self.assertRaises(AssertionError, self.eqFreqs.calcFreqs)
-	
-	
 	############################# by=codon tests ##################################
-	
 	def test_EqualFreqs_calcFreqs_bycodon_typecodon(self):
 		correct = np.array(np.repeat(1./61., 61))
 		self.eqFreqs = EqualFreqs( by = 'codon', type = 'codon' )
@@ -182,143 +153,11 @@ class stateFreqs_EqualFreqs_Tests(unittest.TestCase):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class stateFreqs_UserFreqs_Tests(unittest.TestCase):
 	''' Set of "unittests" for the UserFreqs subclass of StateFreqs.'''
 	
 	def setUp(self):
 		self.dec = 8 # For accuracy
-
-
-	########### incorrect dictionaries provided. should raise assertions. ##############
-	def test_UserFreqs_calcFreqs_bycodon_badkey_length(self):
-		self.uFreqs = UserFreqs(by = 'codon', freqs = {'A':1.0})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-	
-	def test_UserFreqs_calcFreqs_byamino_badkey_length(self):
-		self.uFreqs = UserFreqs(by = 'amino', freqs = {'AAA':1.0})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-		
-	def test_UserFreqs_calcFreqs_bycodon_keytoolong(self):
-		self.uFreqs = UserFreqs(by = 'codon', freqs = {'AAAAAA':1.0})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-		
-	def test_UserFreqs_calcFreqs_bycodon_keytooshort(self):
-		self.uFreqs = UserFreqs(by = 'codon', freqs = {'AA':1.0})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-	
-	def test_UserFreqs_calcFreqs_byamino_keytoolong(self):
-		self.uFreqs = UserFreqs(by = 'amino', freqs = {'AA':1.0})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-		
-	def test_UserFreqs_calcFreqs_bycodon_badkey_letters(self):
-		self.uFreqs = UserFreqs(by = 'codon', freqs = {'ABC':1.0})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-	
-	def test_UserFreqs_calcFreqs_bycodon_badkey_numbers(self):
-		self.uFreqs = UserFreqs(by = 'codon', freqs = {123:1.0})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-	
-	def test_UserFreqs_calcFreqs_bycodon_badvalues_toosmall(self):
-		self.uFreqs = UserFreqs(by = 'codon', freqs = {'AAA':0.5})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-	
-	def test_UserFreqs_calcFreqs_bycodon_badvalues_toobigsingle(self):
-		self.uFreqs = UserFreqs(by = 'codon', freqs = {'AAA':1.5})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-	
-	def test_UserFreqs_calcFreqs_bycodon_badvalues_toobigmultiple(self):
-		self.uFreqs = UserFreqs(by = 'codon', freqs = {'AAA':0.7, 'AAT':0.5})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-		
-	def test_UserFreqs_calcFreqs_bycodon_badvalues_zero(self):
-		self.uFreqs = UserFreqs(by = 'codon', freqs = {'AAA':0})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-	
-	def test_UserFreqs_calcFreqs_bycodon_badvalues_negativedecimal(self):
-		self.uFreqs = UserFreqs(by = 'codon', freqs = {'AAA':-0.5})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-	
-	def test_UserFreqs_calcFreqs_bycodon_badvalues_negativeaboveabs1(self):
-		self.uFreqs = UserFreqs(by = 'codon', freqs = {'AAA':-2})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-
-
-	################## do not provide by or type. assertions shoud be raised. ####################
-
-	def test_UserFreqs_calcFreqs_noby_notype_badtriplet(self):
-		self.uFreqs = UserFreqs(freqs = {'WWW':1.0})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-	
-	def test_UserFreqs_calcFreqs_noby_notype_badsingleletter(self):
-		self.uFreqs = UserFreqs(freqs = {'X':1.0})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-
-	def test_UserFreqs_calcFreqs_noby_notype_ambignucaa(self):
-		self.uFreqs = UserFreqs(freqs = {'A':0.25, 'C':0.25, 'G':0.25, 'T':0.25})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-
-
-	############ provide type but not by. assertions should be raised. ##################
-	def test_UserFreqs_calcFreqs_noby_aminotype_wrongdict_singlecodon(self):
-		self.uFreqs = UserFreqs(type = 'amino', freqs = {'GCA':1.0})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-
-	def test_UserFreqs_calcFreqs_noby_aminotype_wrongdict_multiplecodons(self):
-		self.uFreqs = UserFreqs(type = 'amino', freqs = {'GCA':0.5, 'AAA':0.5})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-
-	def test_UserFreqs_calcFreqs_noby_nuctype_wrongdict_singlecodon(self):
-		self.uFreqs = UserFreqs(type = 'nuc', freqs = {'GCA':1.0})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-	
-	def test_UserFreqs_calcFreqs_noby_nuctype_wrongdict_multiplecodons(self):
-		self.uFreqs = UserFreqs(type = 'nuc', freqs = {'GCA':0.5, 'AAA':0.5})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-		
-	def test_UserFreqs_calcFreqs_noby_codontype_wrongdict_singleaa(self):
-		self.uFreqs = UserFreqs(type = 'codon', freqs = {'W':1.0})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-	
-	def test_UserFreqs_calcFreqs_noby_codontype_wrongdict_multipleaa(self):
-		self.uFreqs = UserFreqs(type = 'codon', freqs = {'W':0.5, 'D':0.5})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-		
-	def test_UserFreqs_calcFreqs_noby_codontype_wrongdict_ambignucaa(self):
-		self.uFreqs = UserFreqs(type = 'codon', freqs = {'A':0.5, 'T':0.5})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-			
-	def test_UserFreqs_calcFreqs_noby_notype_baddict_singleambig(self):
-		self.uFreqs = UserFreqs(freqs = {'A':1.0})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-	
-	def test_UserFreqs_calcFreqs_noby_notype_baddict_multipleambig(self):
-		self.uFreqs = UserFreqs(freqs = {'A':0.5, 'C':0.5})
-		self.assertRaises(AssertionError, self.uFreqs.calcFreqs)
-    
-    
 	
 	############# do not provide by, but everything should work out ok ###################
 	def test_UserFreqs_calcFreqs_noby_notype_gooddict_singlecodon(self):
@@ -402,7 +241,6 @@ class stateFreqs_UserFreqs_Tests(unittest.TestCase):
 		freqs = self.uFreqs.calcFreqs()
 		np.testing.assert_array_almost_equal(correct, freqs, decimal = self.dec, err_msg = "UserFreqs not calculated properly for no by provided with correct codon data.")
 	
-	
 	def test_UserFreqs_calcFreqs_bycodon_typecodon_gooddict_singlecodon(self):
 		correct = np.zeros(61)
 		correct[1] = 1.0
@@ -420,69 +258,6 @@ class stateFreqs_UserFreqs_Tests(unittest.TestCase):
 	
     
     
-  
-
-class stateFreqs_ReadFreqs_Tests(unittest.TestCase):
-	''' Set of "unittests" for the ReadFreqs subclass of StateFreqs.'''
-	
-	def setUp(self):
-		self.dec = 8 # For accuracy
-
-
-	########### file/column specifications which should raise assertions ##############
-
-	def test_ReadFreqs_sanityCheck_bycodon_nocol_badfile(self):
-		self.rFreqs = ReadFreqs(by = 'codon', file = 'NonexistentFilename.txt')
-		self.assertRaises(AssertionError, self.rFreqs.sanityCheck)
-	
-	def test_ReadFreqs_sanityCheck_byamino_nocol_badfile(self):
-		self.rFreqs = ReadFreqs(by = 'amino', file = 'NonexistentFilename.txt')
-		self.assertRaises(AssertionError, self.rFreqs.sanityCheck)
-	
-	def test_ReadFreqs_sanityCheck_bycodon_nocol_aminofile(self):
-		self.rFreqs = ReadFreqs(by = 'codon', file = 'freqFiles/testFreq_amino_aln.fasta')
-		self.assertRaises(AssertionError, self.rFreqs.sanityCheck)
-	
-	def test_ReadFreqs_sanityCheck_bynuc_nocol_aminofile(self):
-		self.rFreqs = ReadFreqs(by = 'nuc', file = 'freqFiles/testFreq_amino_aln.fasta')
-		self.assertRaises(AssertionError, self.rFreqs.sanityCheck)
-
-	def test_ReadFreqs_sanityCheck_byamino_nocol_codonfile(self):
-		self.rFreqs = ReadFreqs(by = 'amino', file = 'freqFiles/testFreq_codon_aln.fasta')
-		self.assertRaises(AssertionError, self.rFreqs.sanityCheck)
-	
-	def test_ReadFreqs_sanityCheck_byamino_nocol_bonkersfile(self):
-		self.rFreqs = ReadFreqs(by = 'amino', file = 'freqFiles/testFreq_bonkers_letters_aln.fasta')
-		self.assertRaises(AssertionError, self.rFreqs.sanityCheck)
-	
-	def test_ReadFreqs_sanityCheck_bynuc_nocol_bonkersfile(self):
-		self.rFreqs = ReadFreqs(by = 'nuc', file = 'freqFiles/testFreq_bonkers_letters_aln.fasta')
-		self.assertRaises(AssertionError, self.rFreqs.sanityCheck)
-		
-	def test_ReadFreqs_sanityCheck_byamino_goodcol_notaln(self):
-		self.rFreqs = ReadFreqs(by = 'amino', columns[0,1,2], file = 'freqFiles/testFreq_amino_notaln.fasta')
-		self.assertRaises(AssertionError, self.rFreqs.sanityCheck)
-	
-	def test_ReadFreqs_sanityCheck_bycodon_goodcol_notaln(self):
-		self.rFreqs = ReadFreqs(by = 'codon', columns=[0,1,2], file = 'freqFiles/testFreq_codon_notaln.fasta')
-		self.assertRaises(AssertionError, self.rFreqs.sanityCheck)
-	
-	def test_ReadFreqs_sanityCheck_bycodon_badcol_colstring(self):
-		self.rFreqs = ReadFreqs(by = 'amino', columns = "thisisnotacolumnlist", file = 'freqFiles/testFreq_codon_aln.fasta')
-		self.assertRaises(AssertionError, self.rFreqs.sanityCheck)
-	
-	def test_ReadFreqs_sanityCheck_bycodon_badcol_colfloat(self):
-		self.rFreqs = ReadFreqs(by = 'amino', columns = 8.345193, file = 'freqFiles/testFreq_codon_aln.fasta')
-		self.assertRaises(AssertionError, self.rFreqs.sanityCheck)
-
-	### above here are all ok.
-	# I still need to confirm that the columns selected are actually within range in the alignment (are there that many columns...)
-	
-   
-    
-    
-    
-    
     
     
     
@@ -490,14 +265,15 @@ class stateFreqs_ReadFreqs_Tests(unittest.TestCase):
     
 if __name__ == '__main__':
 	run_tests = unittest.TextTestRunner()
-	#test_suite_Equal = unittest.TestLoader().loadTestsFromTestCase(stateFreqs_EqualFreqs_Tests)
-	#run_tests.run(test_suite_Equal)
-	#test_suite_Rand = unittest.TestLoader().loadTestsFromTestCase(stateFreqs_RandFreqs_Tests)
+	test_suite_Equal = unittest.TestLoader().loadTestsFromTestCase(stateFreqs_EqualFreqs_Tests)
+	run_tests.run(test_suite_Equal)
+	
+	c#test_suite_Rand = unittest.TestLoader().loadTestsFromTestCase(stateFreqs_RandFreqs_Tests)
 	#run_tests.run(test_suite_Rand)
 	#test_suite_User = unittest.TestLoader().loadTestsFromTestCase(stateFreqs_UserFreqs_Tests)
 	#run_tests.run(test_suite_User)
-	test_suite_Read = unittest.TestLoader().loadTestsFromTestCase(stateFreqs_ReadFreqs_Tests)
-	run_tests.run(test_suite_Read)
+	#test_suite_Read = unittest.TestLoader().loadTestsFromTestCase(stateFreqs_ReadFreqs_Tests)
+	#run_tests.run(test_suite_Read)
 	
 	
 	
