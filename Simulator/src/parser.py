8d2f2058fa6030f0087d9729cd01bca895392c34
Stephanie J Spielman <stephanie.spielman@gmail.com>
2014-05-23 17:21:19 -0500

    more parsing. possibly getting unwieldy(?)

--- Simulator/src/parser.py
+++ Simulator/src/parser.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import re
+from Bio import SeqIO
 from string import whitespace
 from misc import Model, Genetics
 from stateFreqs import *
@@ -11,8 +12,30 @@ class parseConfig:
 	''' Parse temp_config.txt file '''
 	def __init__(self, **kwargs):
 		self.configFile = kwargs.get('file', '../temp_config.txt')
-		self.options = {} # nested dictionaries of options.
-		self.model = Model()
+		self.molecules = Genetics()
+		self.model = Model()   # defined in self.configModelParams
+		self.freqObject = None # defined in self.configModelFrequencies
+		
+		# Nested dictionary of options. Will all go to None, by default, here, and any provided options will be overridden.
+		
+		####### THESE OPTIONS ARE HARD-CODED THROUGHOUT THIS SCRIPT. THEY CANNOT BE CHANGED. ########
+		self.options = {'treefile': None, 
+						'modelClass': None, 
+						'equilibriumFrequencies': None, 
+						'freqBy':None, 
+						'saveFrequencies': None,
+						'freqConstraint': None,
+						'mu':None, 
+						'kappa': None, 
+						'omega': None, 
+						'alpha': None, 
+						'beta': None, 
+						'numberPartitions': None, 
+						'partitionSize': None, 
+						'outpath': None, 
+						'outfile': None
+					   }
+	
 		# Parse the config file into the dictionary, self.options
 		confile = open(self.configFile, 'rU')
 		parsed = confile.readlines()
@@ -21,22 +44,30 @@ class parseConfig:
 			newline = line.translate(None, whitespace)
 			if newline != '':
 				self.options[newline.split('=')[0]] = newline.split('=')[1]
-		
-		# Following two variables are assigned in the function self.configModel() based on the modelClass parameter
-		# They may be overwritten based on user specifications in ...
-		self.freqType = None
-		self.freqBy   = None
-		
-		# TO DO: will need to check this in some way. 
-		self.saveFreq = None
+
+
+
 	
-		self.molecules = Genetics()
+	
+	def configModel(self):
+		''' Crux function. Will generate the full model object (incl. params and eqfreqs). 
+			Calls other crux-like functions:
+				1. configModelParams
+				2. configModelFrequencies
+				etc?
+		'''
+
+		self.configModelParams()
+		self.configModelFrequencies()
+		print self.model.params
+		#print self.freqObject
+		
 	
 	#########################################################################################################################
 	################################### CONFIGURE THE EVOLUTIONARY MODEL PARAMETERS #########################################
 	#########################################################################################################################
 
-	def configModel(self):
+	def configModelParams(self):
 		''' Configure the default evolutionary model specifications.
 			self.options['modelClass'] should be either mutsel, codon, amino, nucleotide.
 			Depending on which model type, construct default model specifications. 
@@ -47,31 +78,34 @@ class parseConfig:
 			self.model.params = {'alpha': 1., 'beta': 1., 'mu':{'AC': 1., 'CA':1., 'AG': 1., 'GA':1., 'AT': 1., 'TA':1., 'CG': 1., 'GC':1., 'CT': 1., 'TC':1., 'GT': 1., 'TG':1.} }
 			self.parseCodonParams()
 			self.freqType = 'codon'
-			self.freqBy = 'amino'
 		
 		elif self.options['modelClass'] == 'mutsel':
 			self.model.params = {'mu':{'AC': 1., 'CA':1., 'AG': 1., 'GA':1., 'AT': 1., 'TA':1., 'CG': 1., 'GC':1., 'CT': 1., 'TC':1., 'GT': 1., 'TG':1.}}
 			self.parseMuKappa()
 			self.freqType = 'codon'
-			self.freqBy = 'amino'
 
 		elif self.options['modelClass'] == 'nucleotide':
 			self.model.params = {'mu':{'AC': 1., 'CA':1., 'AG': 1., 'GA':1., 'AT': 1., 'TA':1., 'CG': 1., 'GC':1., 'CT': 1., 'TC':1., 'GT': 1., 'TG':1.}}
 			self.parseMuKappa()
 			self.freqType = 'nuc'
-			self.freqBy = 'nuc'
 			
 		elif self.options['modelClass'] == 'amino':
 			self.model.params = {'myparams': 'fill in later once this class is constructed.'}
 			self.freqType = 'amino'
-			self.freqBy = 'amino'
 			
 		else:
 			raise AssertionError("You must provide a model class (codon, mutsel, nucleotide, or amino).")
 
-
-
-
+		## CONFIGURE THE self.freqBy
+		self.freqBy = self.options['freqBy']
+		if self.freqBy is None:
+			if self.options['modelClass'] != 'nuc':
+				self.freqBy = 'amino'
+			else:
+				self.freqBy = 'nuc'
+		self.sanityByType()
+		
+		
 	def parseAminoParam(self):
 		''' Write this later etc.'''
 		stephanie = 'awesome'
@@ -80,19 +114,19 @@ class parseConfig:
 	def parseMuKappa(self):
 		''' codon, mutsel, and nucleotide all use mu and kappa. Use this function for all three.'''
 	
-		if "mu" in self.options:
+		if self.options['mu'] is not None:
 			try:
 				self.options_mu = eval(self.options['mu'])
 			except:
 				raise ValueError("\nYour mutational parameters do not appear to be numerical.\n")
-			for k in self.options_mu:
+			for key in self.options_mu:
 				try:
-					provided = float(self.options_mu[k])
-					self.model.params['mu'][k] = provided
+					provided = float(self.options_mu[key])
+					self.model.params['mu'][key] = provided
 				except:
 					raise ValueError("\nYou must provide numeric values for any mutational parameters.\n")		
 		## NOT YET CLEAR TO ME IF THIS SHOULD BE AN IF OR ELSE!!!!!!!! ARE THESE MUTUALLY EXCLUSIVE? NEED TO THINK ON IT.
-		if "kappa" in self.options:
+		if self.options['kappa'] is not None:
 			try:
 				kappa = float(self.options['kappa'])
 			except:
@@ -109,13 +143,13 @@ class parseConfig:
 		'''
 		
 		# Check that reasonable combinations are self.options. Expand on this part later.
-		if "alpha" in self.options and "omega" in self.options:
-			print "\n\nDANGER, WILL ROBINSON!: You specified value(s) for alpha (dS) and/or beta(dN) in addition to omega (dN/dS). Therefore, I will only pay attention to your self.options omega value.\n\n"
+		if self.options['omega'] is not None and (self.options['alpha'] is not None or self.options['beta'] is not None):
+			print "\n\nDANGER, WILL ROBINSON!: You specified value(s) for alpha (dS) and/or beta(dN) in addition to omega (dN/dS). Therefore, I will only pay attention to your specified omega value.\n\n"
 		
 		# Grab any user-provided mu and/or kappa parameters
 		self.parseMuKappa()
 		
-		if "omega" in self.options:
+		if self.options['omega'] is not None:
 			try:
 				omega = float(self.options['omega'])
 			except:
@@ -124,131 +158,118 @@ class parseConfig:
 			self.model.params['alpha'] = 1.0	
 		
 		else:
-			if "alpha" in self.options:
+			if self.options['alpha'] is not None:
 				try:
 					alpha = float(self.options['alpha'])
 				except:
 					raise ValueError("\nIf you wish to provide an alpha (equivalent to dS) value, it must be numeric.\n")
 				self.model.params['alpha'] = alpha
 			
-			if "beta" in self.options:
+			if self.options['beta'] is not None:
 				try:
 					beta = float(self.options['beta'])
 				except:
 					raise ValueError("\nIf you wish to provide a beta (equivalent to dN) value, it must be numeric.\n")
 				self.model.params['beta'] = beta	
+	
+	def sanityByType(self):
+		''' Ensure that freqBy and freqType are fully compatible.'''
+		if self.freqBy == 'nuc' and self.freqType != 'nuc':
+			print "CAUTION: If calculations are performed with nucleotides, you can only retrieve nucleotide frequencies."
+			print "I'm going to calculate nucleotide frequencies for you."
+			self.freqType = 'nuc'
+		if self.freqBy == 'nuc' and self.freqType == 'amino':
+			print "CAUTION: Amino acid frequencies cannot be calculated from nucleotide frequencies."
+			print "I'm going to calculate your frequencies using amino acid frequencies."
+			self.freqBy = 'amino'
+		if self.freqBy == 'amino' and self.freqType == 'nuc':
+			print "CAUTION: Nucleotide frequencies cannot be calculated from amino acid frequencies."
+			print "I'm going to calculate nucleotide frequencies for you."
+			self.freqBy = 'nuc'
 	#########################################################################################################################
 	#########################################################################################################################
+
+
+
 
 	#########################################################################################################################
 	################################### CONFIGURE THE STATE FREQUENCY OBJECT ################################################
 
-	def configFrequencies(self):
+	def configModelFrequencies(self):
 		''' Configure the default evolutionary model specifications.
 			self.options['frequencyClass'] should be either equal, random, a user dictionary, or an indication to read from a file (TBD how this will work).
 			Construct default frequency specifications (equal) and overwrite as needed.
 			NOTE: for codon and mutation-selection models, default codon frequencies are *equal by amino acid*. 
 		'''
 		
+		## TO DO: incorporate functionality for savefile, other ....
 		try:
-			self.parseFrequencyClass(self.options['freqClass'])
+			self.parseFrequencyClass( freqClass = self.options['equilibiumFreqs'] )
+
 		except KeyError:
 			self.freqObject = EqualFreqs(by = self.freqBy, type = self.freqType)
 		
 	
-	def parseFrequencyClass(self, freqClass):
+	def parseFrequencyClass(self, **kwargs):
 		''' Define state frequency instance based on user-provided specifications.
 			Sanity checking is also performed here.
+			TO DO: SAVEFREQS FILE SHOULD BE INCORPORATED IN SOME WAY
 		'''
 		
-		# TO DO: self.saveFreqs should be incorporated in some way
-		if freqClass == 'equal':
+		freqClass = kwargs.get('freqClass')
+		saveFile  = kwargs.get('saveFile', None)
+		
+		# Equal frequencies
+		if freqClass.lower() == 'equal':
 			self.freqObject = EqualFreqs(by = self.freqBy, type = self.freqType)
-		elif freqClass == 'random':
+		
+		# Random frequencies
+		elif freqClass.lower() == 'random':
 			self.freqObject = RandFreqs(by = self.freqBy, type = self.freqType)
+		
+		# User-provided dictionary of frequencies. Constraint may be specified here.
 		elif type(freqClass) == dict:
 			userDict = eval(freqClass)
 			userDict = self.sanityUserFreqs(userDict)
 			self.freqObject = UserFreqs(by = self.freqBy, type = self.freqType, freqs = userDict)
-		else:
-			# Check type
-			if type(freqClass) == str:
-				
-				# Here, we are seeing if they have provided a file. If so, sanity check it. Note that they must also provide a format for this file (fasta, phylip, nexus...)
-				if os.path.exists(freqClass):
-					
-					
-					
-			# could be a file name - check if so here. This should be something dynamic because you can't look for a dictionary (above) and then yell if not a dictionary.
-			# DEAL WITH THIS!!
-			stephanie = 'really awesome'
-			 
-	
-	def sanityReadFreqs(self, freqClass):
-		''' Sanity check the frequency specification if a file was provided.'''
+			if self.options['freqConstraint']:
+				constraint = self.sanityReadFreqs_checkConstraint()
+				self.freqObject.constraint = constraint
 		
-	
-	
-	def sanityReadFreqs_checkAlphabet(self):	
-		''' Ensure sequences in file provided are the correct alphabet. ''' 
-		fullSeq = "".join(self.seqs)
-		fullSeq = fullSeq.translate(None, '-?.*BJOUXZ') # Remove ambiguities
-		fullLength = len(fullSeq)
-		DNA = re.compile(r"[ACGT]")
-		PROT = re.compile(r"[ACDEFGHIKLMNPQRSTVWY]")
-		# Double check that everything in this culled dataset is a correct character given intended alphabets
-		dnaChar = len(DNA.findall(fullSeq))
-		protChar = len(PROT.findall(fullSeq))
-		#print dnaChar, protChar, fullLength
-		if self.by == 'amino':
-			assert(dnaChar != protChar), "Are you sure this is a protein sequence file?"
-			assert(protChar == fullLength), "Your sequences do not appear to be the correct alphabet for your frequency calculations, or you have bizarre characters in the sequences."
-		elif self.by == 'codon' or self.by == 'nuc':
-			assert(dnaChar == fullLength), "Your sequences do not appear to be the correct alphabet for your frequency calculations, or you have bizarre characters in the sequences."
-
-	def sanityReadFreqs_checkColumns(self):
-		''' Sanity checking for proper column specification, ONLY if user has requested frequencies come from columns.
-			1. Columns must be a list. We can work with a single int or tuple, but that's it.
-			2. Sequences must be an alignment if columns are requested.
-			3. There actually need to be that many columns in the alignment. As in, if user requests column #20, there better be a column #20.
-		'''
-		# Ensure self.whichCol is a list.
-		if type(self.whichCol) is not list:
-			if type(self.whichCol) is int:
-				self.whichCol = [self.whichCol]
-			if type(self.whichCol) is tuple:
-				self.whichCol = list(self.whichCol)
+		# File from which to read frequencies. Constraint may be specified here.
+		elif os.path.exists(freqClass):
+			# Check if columns were also specified
+			if self.options['columns']:
+				userColumns = self.options['columns']
 			else:
-				raise AssertionError("If you'd like to read frequencies by column, you must provide a *list* of which columns (indexed at 0) you want.")	
+				userColumns = None
+			seqfile_ext, final_columns = self.sanityReadFreqs(freqClass, userColumns)
+			self.freqObject = ReadFreqs(by = self.freqBy, type = self.freqType, file = freqClass, format = seqfile_ext, columns = final_columns)
+			if self.options['freqConstraint']:
+				constraint = self.sanityReadFreqs_checkConstraint()
+				self.freqObject.constraint = constraint
+
+		else:
+			raise AssertionError("\n\nIncorrect specification for equilibrium frequency.\n\n") 
+	
+		####### TO DO ##########
+		# Any other frequency options?? For instance, savefile????????
+		if saveFile:
+			self.freqObject.savefile = saveFile
+
+	
+	
+	def sanityReadFreqs_checkConstraint(self):
+		'''If a constraint is specified, must be float between 0-1.'''
+		try:
+			constraint = float(self.options['freqConstraint'])
+		except ValueError:
+			print "To specify a constraint on equilibrium frequencies, provide a decimal in the range (0,1]."
+		assert(constraint > 0. and constraint <= 1.), "To specify a constraint on equilibrium frequencies, provide a decimal in the range (0,1]."
+		return constraint
 		
-		# Ensure sequences were an alignment
-		if self.whichCol is not None:
-			for i in range(1, self.numseq):
-				seqlen = len(self.rawrecords[i].seq)
-				assert(seqlen == self.alnlen), "Your provided file does not appear to be an alignment. If you want to collect frequency information from columns, it *must* be an alignment!!"	
 		
-		# Ensure that indexing is within range. 
-		assert(max(self.whichCol) < self.alnlen), "Your provided alignment does not contain the columns you requested."	
-			
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		
 	def sanityUserFreqs(self, userDict):
 		''' Sanity check to ensure that the dictionary provided is compatible with other specifications.'''
 		if self.freqBy == 'codon':
@@ -271,20 +292,108 @@ class parseConfig:
 			sum += userDict[key.upper()]
 		assert ( abs(sum - 1.) < self.zero), ("\n\nIf you provide frequencies, they must sum to 1. The provided frequencies sum to",sum,".")
 		return userDict
-			
-		
-	#########################################################################################################################
-	#########################################################################################################################
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	def sanityReadFreqs(self, seqfile, userColumns):
+		''' Sanity check the frequency specification if a file was provided.
+			Return a proper column list, if exists, and sequence file extension.		
+		'''
 		
+		# Ensure that the file can be parsed. 
+		seqs, ext = self.sanityReadFreqs_readSeqFile(seqfile)
+
+		# Ensure correct alphabet
+		self.sanityReadFreqs_checkAlphabet(seqs)
 		
+		# If columns provided, ensure correct format
+		if userColumns:
+			columns = self.sanityReadFreqs_checkColumns(seqs, userColumns)
+	
+		return (ext, userColumns)
+	
+	
+	
+	def sanityReadFreqs_readSeqFile(self, seqfile):
+		''' Read in a sequence file. Also return the format the file is in, if we can parse it.'''
+		# Accepted formats are fasta, phylip, clustal, nexus, stockholm. 
+		exts = ['fasta', 'phylip', 'clustal', 'nexus', 'stockholm']
+		for ext in exts:
+			try:
+				records = list(SeqIO.parse(file, ext))
+			except:
+				pass
+			if records:
+				break
+		if records is None:
+			raise AssertionError("\n\n I'm having trouble reading the file from which I need to calculated equilibrium frequencies.\n Please specify a file in either fasta, phylip, nexus, clustal, or stockholm format.")
+		seqs = []
+		for record in records:
+			seqs.append(str(record.seq))
+		return (seqs, ext)
+	
+	
+	
+	
+	
+	def sanityReadFreqs_checkAlphabet(self, seqs):	
+		''' Ensure sequences in file provided are the correct alphabet. ''' 
+		fullSeq = "".join(seqs)
+		fullSeq = fullSeq.translate(None, '-?.*BJOUXZ') # Remove ambiguities
+		fullLength = len(fullSeq)
+		DNA = re.compile(r"[ACGT]")
+		PROT = re.compile(r"[ACDEFGHIKLMNPQRSTVWY]")
+		dnaChar = len(DNA.findall(fullSeq))
+		protChar = len(PROT.findall(fullSeq))
+		if self.freqBy == 'amino':
+			assert(dnaChar != protChar), "Are you sure this is a protein sequence file?"
+			assert(protChar == fullLength), "Your sequences do not appear to be the correct alphabet for your frequency calculations, or you have bizarre characters in the sequences."
+		elif self.freqBy == 'codon' or self.freqBy == 'nuc':
+			assert(dnaChar == fullLength), "Your sequences do not appear to be the correct alphabet for your frequency calculations, or you have bizarre characters in the sequences."
+
+
+
+
+	def sanityReadFreqs_checkColumns(self, seqs, userColumns):
+		''' Sanity checking for proper column specification, ONLY if user has requested frequencies come from columns.
+			1. Columns must be a list. We can work with a single int or tuple, but that's it.
+			2. Sequences must be an alignment if columns are requested.
+			3. There actually need to be that many columns in the alignment. As in, if user requests column #20, there better be a column #20.
+		'''
+		# Ensure columns is a list.
+		if type(userColumns) != list:
+			if type(userColumns) == tuple:
+				columns = list(userColumns)
+			elif type(userColumns) == str:
+				columns = eval(userColumns)
+			else:	
+				raise AssertionError("If you'd like to read frequencies by column, you must provide a list of which columns (indexed at 0) you want.")	
+		else:
+			columns = userColumns
 		
+		# Ensure columns contains only integers
+		for col in columns:	
+			assert(type(col) == int), "You must provide integer values for any columns you want to specify when collecting equilibrium frequencies."
+	
+		# Ensure sequences were an alignment
+		alnlen = len(seqs[0])
+		for i in range(1, len(seqs)):
+			seqlen = len(seqs[i])
+			assert(seqlen == alnlen), "\nIf you want to collect frequency information from columns, your specified sequence file *must* be an alignment. Currently, it is not.\n"	
 		
-		
-		
-					
-				
-				
+		# Ensure that indexing is within range. 
+		assert(min(columns) >= 0 and max(columns) < alnlen), "Your provided alignment does not contain the columns you requested. Remember that you should index at 0."	
+			
 			
 
 conf = parseConfig()
