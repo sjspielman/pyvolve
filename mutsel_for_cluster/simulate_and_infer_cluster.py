522519569bff11841bfe81a01cca721e41357289
Stephanie J Spielman <stephanie.spielman@gmail.com>
2014-05-26 12:51:05 -0500

    better conditions, uses counting method for dnds

--- mutsel_for_cluster/simulate_and_infer_cluster.py
+++ mutsel_for_cluster/simulate_and_infer_cluster.py
@@ -18,16 +18,18 @@ from newick import *
 from stateFreqs import *
 from matrixBuilder import *
 from evolver import *
-
+from mutation_counter import *
+from site_counter import *
+from predict_omega import *
 from functions_simandinf import *
 
 ################################ GLOBAL #######################################
 molecules = Genetics()
-treefile = "100.tre" # in here, all branch lengths are 0.5
+treefile = "2.tre" # in here, all branch lengths are 0.5
 my_tree = readTree(file=treefile)
-length = 100
+length = 100000
 codonParams = {'alpha': 1.0, 'mu': {'AC': 1., 'AG': 1., 'AT': 1., 'CG': 1., 'CT': 1., 'GT': 1.}}
-mutSelParams = {'mu': {'AC': 1e-5, 'CA':1e-5, 'AG': 1e-5, 'GA':1e-5, 'AT': 1e-5, 'TA':1e-5, 'CG': 1e-5, 'GC':1e-5, 'CT': 1e-5, 'TC':1e-5, 'GT': 1e-5, 'TG':1e-5}}
+mutSelParams = {'mu': {'AC': 0.001, 'CA':0.001, 'AG': 0.001, 'GA':0.001, 'AT': 0.001, 'TA':0.001, 'CG': 0.001, 'GC':0.001, 'CT': 0.001, 'TC':0.001, 'GT': 0.001, 'TG':0.001}}
 ###############################################################################
 
 hrh1_columns = [134, 155, 157, 158, 160, 161, 162, 164, 165, 166, 167, 168, 169, 172, 175, 179, 182, 183, 185, 186, 188, 200, 207, 208, 212, 213, 214, 215, 220, 221, 222, 231, 232, 233, 235, 239, 252, 257, 264, 268, 271, 275, 276, 277, 279, 280, 281, 283, 284, 285, 287, 288, 290, 291, 293, 297, 298, 300, 318, 320, 322, 324, 326, 327, 329, 330, 334, 337, 338, 342, 345, 346, 349, 353, 356, 360, 362, 363, 364, 369, 370, 371, 372, 373, 374, 384, 386, 387, 389, 408, 419, 420, 441, 442, 466, 529, 563, 566, 567, 573, 574, 576, 593, 607, 610, 612, 613, 616, 617, 618, 620, 636, 637, 638, 641, 645, 649, 652, 653, 655, 656, 659, 664, 666, 675, 678, 679, 683, 684, 685, 686, 693, 696, 700, 707, 711, 712, 715, 716, 717, 718, 719, 720]
@@ -60,29 +62,40 @@ myEvolver.writeSequences()
 print "Deriving"
 derived_w = deriveAverageOmegaAlignment(seqfile, f)
 
+# Get omega using counting method
+records = list(SeqIO.parse('temp.fasta', 'fasta'))
+s1 = records[0].seq
+s2 = records[1].seq
+( ns_mut, s_mut ) = M.countMutations( s1, s2 )
+( ns_sites2, s_sites2 ) = S.countSites( s2 )
+dS = 2*sum( s_mut )/(sum( s_sites1 ) + sum( s_sites2 ))
+dN = 2*sum( ns_mut )/(sum( ns_sites2 ) + sum( ns_sites2 ))
+count_w = dN/dS
+
+
 
 # Send to HyPhy
-print "hyphy"
-setuphyphy = "cat 100.tre >> temp.fasta"
-setup = subprocess.call(setuphyphy, shell = True)
-assert(setup == 0), "couldn't prep file for hyphy"
-hyphy = "./HYPHYMP globalGY94.bf CPU="+cpu+" > hyout.txt"
-runhyphy = subprocess.call(hyphy, shell = True)
-assert (runhyphy == 0), "hyphy fail"
+#print "hyphy"
+#setuphyphy = "cat 100.tre >> temp.fasta"
+#setup = subprocess.call(setuphyphy, shell = True)
+#assert(setup == 0), "couldn't prep file for hyphy"
+#hyphy = "./HYPHYMP globalGY94.bf CPU="+cpu+" > hyout.txt"
+#runhyphy = subprocess.call(hyphy, shell = True)
+#assert (runhyphy == 0), "hyphy fail"
 
 # grab hyphy output
-hyout = open('hyout.txt', 'r')
-hylines = hyout.readlines()
-hyout.close()
-for line in hylines:
-	findw = re.search("^w=(\d+\.*\d*)", line)
-	if findw:
-		hyphy_w = findw.group(1)
-		break
+#hyout = open('hyout.txt', 'r')
+#hylines = hyout.readlines()
+#hyout.close()
+#for line in hylines:
+#	findw = re.search("^w=(\d+\.*\d*)", line)
+#	if findw:
+#		hyphy_w = findw.group(1)
+#		break
 			
 # Now save everything to file
 outf = open(outfile, 'w')
-outf.write(str(numaa[run]) + '\t' + str(hrh1_columns[run]) + '\t' + str(hyphy_w) + '\t' + str(derived_w) + '\n')
+outf.write(str(numaa[run]) + '\t' + str(hrh1_columns[run]) + '\t' + str(count_w) + '\t' + str(derived_w) + '\n')
 outf.close()
 
 # And now send to the final outfile
