f140095c47b449648ef0bb33a959f18890f5117d
Stephanie J Spielman <stephanie.spielman@gmail.com>
2014-07-04 16:40:03 -0500

    cleanups so any alphabet.

--- Simulator/src/evolver.py
+++ Simulator/src/evolver.py
@@ -1,4 +1,5 @@
 import numpy as np
+import dendropy as dp
 from scipy import linalg
 import random as rn
 import misc
@@ -10,49 +11,65 @@ class Evolver(object):
         #Provided by user
         self.parts    = kwargs.get("partitions", None)   # this should be a list of tuples. Each tuple is (length, model). 
         self.numparts = len(self.parts)
+        assert (len(self.numparts) >= 1), "You have nothing to evolve. Partitions, please!"
         self.tree     = kwargs.get("tree", None)
-        self.outfile  = kwargs.get("outfile", "seqs_"+time.strftime("%m.%d.%H.%M.%S")+".fasta")
+        assert (self.tree is not None), "You have no tree. Treeeeeee!"
+        self.outfile  = kwargs.get("outfile", "seqs_"+time.strftime("%m.%d.%H.%M.%S")+".fasta")        
         self.ratefile = kwargs.get("ratefile", None)
         
         self.seqlen   = 0
         for i in range(self.numparts):
             self.seqlen += self.parts[i][0]
                 
-        #Internals
+        # Internals
         self.alndict = {}
         self.zero = 1e-8
         self.molecules = misc.Genetics()
+        
+        dimensions = self.parts[0][1].Q.shape[0]
+        if dimensions == 4:
+            self.code = self.molecules.nucleotides
+        elif dimsensions == 20:
+            self.code = self.molecules.amino_acids
+        elif dimensions == 61:
+            self.code = self.molecules.codons
+        else:
+            raise AssertionError("Matrix is totally weird.")
+        
+        
+        
 
-    def codon2int(self, codon):
-        ''' Take a codon and return its integer index 0-60 '''
-        codind = self.molecules.codons.index(codon)
-        return codind
+    def seq2int(self, entry):
+        ''' Take a DNA/PROT value and return its integer index 0-60 '''
+        return self.code.index(entry)
     
-    def int2codon(self, codind):
-        ''' Take a codon index (0-60) and return its corresponding codon ''' 
-        codon = self.molecules.codons[codind]
-        return codon
+    
+    def int2seq(self, index):
+        ''' Take a DNA/PROT index (0-60) and return its corresponding letter/codon ''' 
+        return self.code[index]
+    
     
     def intseq_to_string(self, intseq):
         ''' Take a sequence coded as ints and turn to actual codon string '''
         stringseq = ''
         for i in intseq:
-            codon = self.int2codon(i)
-            stringseq += codon
+            single_seq = self.int2seq(i)
+            stringseq += single_seq
         return stringseq
     
         
     def generateRootSeq(self):
+        ''' Select starting sequence based on state frequencies, for each partition. '''
         rootSeq = np.empty(self.seqlen, dtype=int)
         index=0
         for i in range(self.numparts):
             seqlen = self.parts[i][0]
             freqs  = self.parts[i][1].params["stateFreqs"]
             for j in range(seqlen):
-                rootSeq[index] = self.generateCodon(freqs)
+                rootSeq[index] = self.generateSeq(freqs)
                 index += 1
         return rootSeq    
-    
+
 
     def sim_sub_tree(self, tree, baseSeq = None):
         ''' Traverse the tree and simulate. '''
@@ -73,11 +90,11 @@ class Evolver(object):
             self.alndict[tree.name]=tree.seq
     
     
-    def generateCodon(self, probArray):
-        ''' Sample a codon. probArray can be any list/numpy array of probabilities that sum to 1.'''
+    def generateSeq(self, probArray):
+        ''' Sample a sequence letter (nuc,aa,or codon). probArray can be any list/numpy array of probabilities that sum to 1.'''
         #### CHECKED FXN ON 2/6/14. WORKS AS INTENDED #####
         # Assertion is overkill but who cares
-        assert ( abs(np.sum(probArray) - 1.) < self.zero), "Probabilities do not sum to 1. Cannot generate a codon."
+        assert ( abs(np.sum(probArray) - 1.) < self.zero), "Probabilities do not sum to 1. Cannot generate a molecule."
         r = rn.uniform(0,1)
         i=0
         sum=probArray[i]
@@ -86,12 +103,9 @@ class Evolver(object):
             sum+=probArray[i]
         return i        
 
-    def checkBranch(self, node, baseSeq):
+    def checkParentBranch(self, node, baseSeq):
         ''' Check that the baseSeq exists and the branch length is reasonable ''' 
-        ## Check that there is a sequence to evolve from
         assert (baseSeq != None), "There is no parent sequence."
-        
-        # Retrieve branch length
         bl = float( node.branch )
         assert (bl >= 0), "Branch length is negative. Must be >= 0."
         return bl            
@@ -99,7 +113,7 @@ class Evolver(object):
 
     def writeSequences(self):
         ''' Write resulting sequences to a file'''
-        print "Writing sequences to file"
+        print "Writing sequences to file, in fasta format."
         out_handle=open(self.outfile, 'w')
         for entry in self.alndict:
             seq = self.intseq_to_string(self.alndict[entry])
@@ -107,6 +121,7 @@ class Evolver(object):
         out_handle.close()    
         
 
+    ##################### THIS FUNCTION IS TOTALLY DEPRECATED ############################
     def printRates(self):
         ''' Print dN/dS to file for each site. ASSUMES THAT SITES ARE NOT SHUFFLED. ''' 
         
@@ -119,7 +134,7 @@ class Evolver(object):
                 f.write(str(index)+'\t'+partRate+'\n')
                 index+=1
         f.close()    
-    
+    ######################################################################################
     
         
     def evolve_branch(self, node, baseSeq):
@@ -129,22 +144,23 @@ class Evolver(object):
 
 
 class StaticEvolver(Evolver):
-    ''' Evolve according to a static landscape (no temporal variation) ''' 
+    ''' Evolve according to a static landscape (no temporal variation). Site heterogeneity ok, but no temporal/branch heterogeneity. ''' 
+    
     def __init__(self, **kwargs):
         super(StaticEvolver, self).__init__(**kwargs)
     
-    
     def evolve_branch(self, node, baseSeq):
         
-        bl = self.checkBranch(node, baseSeq)
+        bl = self.checkParentBranch(node, baseSeq)
         
         # If there is no branch length then there is nothing to evolve. Attach baseSeq to node
-        if bl < self.zero:
+        if bl <= self.zero:
             print bl, "branch length of 0 detected"
             node.seq = baseSeq
         
         else:
-            ## Evolve for each partition and then join together
+        
+            # Evolve each partition, i, and add onto newSeq as we go
             newSeq = np.empty(self.seqlen, dtype=int)
             index = 0
             for i in range(self.numparts):
@@ -154,14 +170,14 @@ class StaticEvolver(Evolver):
                 instMat = self.parts[i][1].Q
                 
                 # Generate probability matrix for evolution along this branch and assert correct
-                Qt = np.multiply(instMat, bl) # Matrix has already been scaled properly.
+                Qt = np.multiply(instMat, bl)
                 probMatrix = linalg.expm( Qt ) # Generate P(t) = exp(Qt)
-                for i in range(61):
+                for i in range(len(self.code)):
                     assert( abs(np.sum(probMatrix[i]) - 1.) < self.zero ), "Row in P(t) matrix does not sum to 1."
     
                 # Move along baseSeq and evolve. 
                 for j in range(seqlen):
-                    newSeq[index] = self.generateCodon( probMatrix[baseSeq[index]] )
+                    newSeq[index] = self.generateSeq( probMatrix[baseSeq[index]] )
                     index+=1
                     
             # Attach final sequence to node
@@ -176,7 +192,7 @@ class ShiftingEvolver(Evolver):
 
     def evolve_branch(self, node, baseSeq):
         
-        bl = self.checkBranch(node, baseSeq)
+        bl = self.checkParentBranch(node, baseSeq)
         
         # If there is no branch length then there is nothing to evolve. Attach baseSeq to node
         if bl < self.zero:
@@ -201,7 +217,7 @@ class ShiftingEvolver(Evolver):
     
                 # Move along baseSeq and evolve. 
                 for j in range(seqlen):
-                    newSeq[index] = self.generateCodon( probMatrix[baseSeq[index]] )
+                    newSeq[index] = self.generateSeq( probMatrix[baseSeq[index]] )
                     index+=1
                     
             # Attach final sequence to node
