da0a185017e28c75b37017898accc82e850c7358
Stephanie J Spielman <stephanie.spielman@gmail.com>
2014-10-16 15:38:31 -0500

    no need for a rootmodel since no branch heterogeneity in example

--- examples/run_pyvolve.py
+++ examples/run_pyvolve.py
@@ -65,7 +65,6 @@ model1 = Model()
 model1.params = {'omega': 0.5, 'kappa': 2.5, 'state_freqs': frequencies_model1} # Define GY94 model parameters as attributes of the model1 object
 build_matrix1 = mechCodon_Matrix(model1) # Define MatrixBuilder object, AFTER model1.params has been defined!!
 model1.matrix  = build_matrix1.assemble_matrix() # Build the matrix and assign as attribute to model1 object
-model1.name = "m1"
 
 
 # Parameters for the second partition are a little different. To define different rates for dN and dS, we use the keys 'beta' and 'alpha', respectively, following the notation of MuseGaut1994.
@@ -75,14 +74,6 @@ part_mu = {'AC': 1.5, 'AG': 2.6, 'AT': 0.4, 'CG': 1.0, 'CT': 0.004, 'GT': 1.34} 
 model2.params = {'beta': 2.5, 'alpha': 0.75, 'mu': part_mu, 'state_freqs': frequencies_model2}
 build_matrix2 = mechCodon_Matrix(model2) # Define MatrixBuilder object
 model2.matrix  = build_matrix2.assemble_matrix() # Build the matrix and assign as attribute to model2 object
-model2.name = "m2"
-
-rootmodel = Model()
-part_mu = {'AC': 1.5, 'AG': 2.6, 'AT': 0.4, 'CG': 1.0, 'CT': 0.004, 'GT': 1.34} # Here, the rate of A->C and C->A is therefore 1.5, and so on. The keys for this list must be alphabetically ordered, as in "AG" should be used and not "GA".  
-rootmodel.params = {'beta': 2.5, 'alpha': 0.75, 'mu': part_mu, 'state_freqs': frequencies_model2}
-build_matrixr = mechCodon_Matrix(rootmodel) # Define MatrixBuilder object
-rootmodel.matrix  = build_matrixr.assemble_matrix() # Build the matrix and assign as attribute to model2 object
-rootmodel.name = "root_model"
 
 
 # THIRD, we will define our partitions which make use of the models defined above. Each partition is again a Partition() object with several attributes.
