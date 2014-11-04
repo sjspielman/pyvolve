870f1ae0f1982e42e64d7a3e6c4c7acf93e34042
Stephanie J Spielman <stephanie.spielman@gmail.com>
2014-11-04 15:22:18 -0600

    added evolver tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -11,12 +11,33 @@
 import unittest
 from matrix_builder_test import *
 from state_freqs_test import *
+from evolver_test import *
+
 
 if __name__ == '__main__':
 
     run_tests = unittest.TextTestRunner()
     print "Running unit tests for pyvolve"
+
+    ############################## STATE_FREQS TESTS ###########################
     
+    print "Testing the RandomFrequencies subclass of StateFrequencies"
+    test_suite_Rand = unittest.TestLoader().loadTestsFromTestCase(state_freqs_RandomFrequencies_Tests)
+    run_tests.run(test_suite_Rand)
+
+    print "Testing the EqualFrequencies subclass of StateFrequencies"
+    test_suite_Equal = unittest.TestLoader().loadTestsFromTestCase(state_freqs_EqualFrequencies_Tests)
+    run_tests.run(test_suite_Equal)
+
+    print "Testing the CustomFrequencies subclass of StateFrequencies"
+    test_suite_User = unittest.TestLoader().loadTestsFromTestCase(state_freqs_CustomFrequencies_Tests)
+    run_tests.run(test_suite_User)
+
+    print "Testing the ReadFrequencies subclass of StateFrequencies"
+    test_suite_Read = unittest.TestLoader().loadTestsFromTestCase(state_freqs_ReadFrequencies_Tests)
+    run_tests.run(test_suite_Read)
+    
+        
     ##################### MATRIX_BUILDER TESTS #######################
     print "Testing assemble_matrix function of matrixBuilder for codon model"
     test_suite_assemble_matrix = unittest.TestLoader().loadTestsFromTestCase(matrixBuilder_assemble_matrix_tests)
@@ -50,20 +71,22 @@ if __name__ == '__main__':
     test_suite_mutSelMatrix_nuc = unittest.TestLoader().loadTestsFromTestCase(matrixBuilder_mutSel_nuc_Matrix_tests)
     run_tests.run(test_suite_mutSelMatrix_nuc) 
     
-    ############################## STATE_FREQS TESTS ###########################
     
-    print "Testing the RandomFrequencies subclass of StateFrequencies"
-    test_suite_Rand = unittest.TestLoader().loadTestsFromTestCase(state_freqs_RandomFrequencies_Tests)
-    run_tests.run(test_suite_Rand)
+    
+    ############################## EVOLVER TESTS ###########################
+    
+    print "Testing evolver no het, one partition"
+    test_suite0 = unittest.TestLoader().loadTestsFromTestCase(evolver_singlepart_nohet_tests)
+    run_tests.run(test_suite0)
 
-    print "Testing the EqualFrequencies subclass of StateFrequencies"
-    test_suite_Equal = unittest.TestLoader().loadTestsFromTestCase(state_freqs_EqualFrequencies_Tests)
-    run_tests.run(test_suite_Equal)
+    print "Testing evolver no het, two partitions"
+    test_suite1 = unittest.TestLoader().loadTestsFromTestCase(evolver_twopart_nohet_tests)
+    run_tests.run(test_suite1)
 
-    print "Testing the CustomFrequencies subclass of StateFrequencies"
-    test_suite_User = unittest.TestLoader().loadTestsFromTestCase(state_freqs_CustomFrequencies_Tests)
-    run_tests.run(test_suite_User)
-
-    print "Testing the ReadFrequencies subclass of StateFrequencies"
-    test_suite_Read = unittest.TestLoader().loadTestsFromTestCase(state_freqs_ReadFrequencies_Tests)
-    run_tests.run(test_suite_Read)
+    print "Testing evolver site het, one partition"
+    test_suite2 = unittest.TestLoader().loadTestsFromTestCase(evolver_sitehet_tests)
+    run_tests.run(test_suite2)
+    
+    print "Testing evolver branch het, one partition"
+    test_suite3 = unittest.TestLoader().loadTestsFromTestCase(evolver_branchhet_tests)
+    run_tests.run(test_suite3)
