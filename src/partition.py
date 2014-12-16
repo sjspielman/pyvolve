f29235d027c39d99fbecb0e45578d159e692d8c9
Stephanie J Spielman <stephanie.spielman@gmail.com>
2014-12-16 13:36:10 +0200

    kwargs size, models for parttition init

--- src/partition.py
+++ src/partition.py
@@ -18,9 +18,9 @@ class Partition():
     '''
         Defines a Partition() object.
     '''
-    def __init__(self):
-        self.size              = []    # List of integers representing partition length. If there is no rate heterogeneity, then the list is length 1. Else, list is length k, where k is the number of rate categories.
-        self.models            = None  # List of models associated with this partition. When length 1, temporally homogeneous.
+    def __init__(self, **kwargs):
+        self.size              = kwargs.get('size', [])   # List of integers representing partition length. If there is no rate heterogeneity, then the list is length 1. Else, list is length k, where k is the number of rate categories.
+        self.models            = kwargs.get('models', None)  # List of models associated with this partition. When length 1, temporally homogeneous.
         self.root_model_name   = None  # NAME of of Model beginning evolution at root of tree. Used under *branch heterogeneity*, and should be None or False if process is temporally homogeneous. If there is branch heterogeneity, this string *MUST* correspond to one of the Model() object's names and also a corresponding phylogeny flag.
         self.root_seq          = None  # User may choose to provide a root sequence for each partition, and it'll be stored here. Totally optional - will otherwise be generated from steady-state frequencies.
         self.shuffle           = False # Shuffle sites after evolving? 
