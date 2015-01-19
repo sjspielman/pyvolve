f993607e0ac13461978b31e1cc2f5b5c7c8f2ce7
Stephanie J Spielman <stephanie.spielman@gmail.com>
2015-01-19 13:37:02 -0600

    partition doc update

--- src/partition.py
+++ src/partition.py
@@ -7,7 +7,7 @@
 ##############################################################################
 
 '''
-    Define Partition() class.
+    This module defines the Partition() class, which indicates a particular evolutionary unit. 
 '''
 
 from model import * 
@@ -15,21 +15,36 @@ from model import *
 
 
 class Partition():
-    '''
-        Defines a Partition() object.
-    '''
+
     def __init__(self, **kwargs):
+        '''
+            Required keyword arguments:
+                
+                1. **size**, integer giving the root length of this partition
+                2. **models**, either a single Model/CodonModel instance (for cases of branch homogeneity), or a list of Model/CodonModel instances (for cases of branch heterogeneity)
+        
+            Examples:
+            .. code-block:: python
+
+                >>> # Define a temporally homogeneous partition
+                >>> my_partition = Partition(size = 500, my_model) 
+                
+                >>> # Define a temporally heterogeneous partition, in which three models (model1, model2, and model3) are used during sequence evolution.
+                >>> my_other_partition = Partition(size = 134, [model1, model2, model3])       
+                
+        '''
+                
+                
         self.size              = kwargs.get('size', [])   # List of integers representing partition length. If there is no rate heterogeneity, then the list is length 1. Else, list is length k, where k is the number of rate categories.
         self.models            = kwargs.get('models', None)  # List of models associated with this partition. When length 1, temporally homogeneous.
         self.root_model_name   = None  # NAME of of Model beginning evolution at root of tree. Used under *branch heterogeneity*, and should be None or False if process is temporally homogeneous. If there is branch heterogeneity, this string *MUST* correspond to one of the Model() object's names and also a corresponding phylogeny flag.
-        self.root_seq          = None  # User may choose to provide a root sequence for each partition, and it'll be stored here. Totally optional - will otherwise be generated from steady-state frequencies.
-        self.shuffle           = False # Shuffle sites after evolving? 
+        self.shuffle           = False # Shuffle sites after evolving? The evolver module will set this up.
         self._root_model       = None  # The actual root model. Used internally in evolver module.
 
     
     def branch_het(self):
         ''' 
-            Return False if there is no branch heterogeneity, True if there is.
+            Return True if the partition uses branch heterogeneity, and False if homogeneous.
         '''
         if isinstance(self.models, Model) or isinstance(self.models, CodonModel) or len(self.models) == 1:
             return False
@@ -40,7 +55,7 @@ class Partition():
 
     def site_het(self):
         ''' 
-            Return False if there is no site heterogeneity, True if there is.
+            Return True if the partition uses site/rate heterogeneity, and False if homogeneous.
         '''
         m = self.models[0]
         if m.num_classes() > 1:
@@ -51,7 +66,7 @@ class Partition():
     
     def codon_model(self):
         '''
-            Return True if Partition is evolving with CodonModel objects (dN/dS model heterogeneity) and False otherwise.
+            Return True if Partition is evolving according to CodonModel objects (dN/dS model heterogeneity), and False if Model objects.
         '''
         if isinstance(self.models[0], CodonModel):
             return True
