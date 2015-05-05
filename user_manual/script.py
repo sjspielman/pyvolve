be839eb7f8925d31263f263308826905da2811a5
Stephanie Spielman <stephanie.spielman@gmail.com>
2015-05-05 12:33:28 -0500

    manual update

--- user_manual/script.py
+++ user_manual/script.py
@@ -1,14 +1,14 @@
 from pyvolve import *
 
 # Read in a newick tree
-t = read_tree(file = "myfile.tre")
+t = read_tree(tree = "(t4:0.785,(t3:0.380,(t2:0.806,(t5:0.612,t1:0.660):0.762):0.921):0.207);")
 
 # Construct state frequency vector. Optional!
 f = EqualFrequencies("amino")
 freqs = f.construct_frequencies(type = "codon")
 
 # Build the evolutionary model
-m = Model("GY94", {'state_freqs':freqs, 'omega':1.5, kappa:3.4}
+m = Model("GY94", {'state_freqs':freqs, 'omega':1.5, 'kappa':3.4})
 m.construct_model()
 
 # Initialize partitions
