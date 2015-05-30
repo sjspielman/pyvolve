126f7c227f95fd9cc0147cd7f31b1b51cd465e66
sjspielman <stephanie.spielman@utexas.edu>
2015-05-30 13:19:14 -0500

    doc update, and theme change

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,8 @@
 
 import sys
 import os
+import alabaster
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -106,7 +108,10 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
