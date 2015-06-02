751513d38b01b609994f143d22b3c80a4b024387
Stephanie Spielman <stephanie.spielman@gmail.com>
2015-06-02 18:43:39 -0500

    custom code example

---
+++ examples/custom_code.py
@@ -0,0 +1,17 @@
+# This example script demonstrates how to evolve according to custom model with custom code
+
+import pyvolve
+import numpy as np
+
+# Define a phylogeny, from a file containing a newick tree
+my_tree = pyvolve.read_tree(file = "examples/file_with_tree.tre")
+
+# Define a custom model with custom matrix and custom code (states). The matrix must be square and have the same dimension (in 1D) as the provided code. Note that code is a list because, in theory, you can specify multi-character (as in letters) states.
+matrix = np.array([ [-0.5, 0.25, 0.25], [0.25, -0.5, 0.25], [0.25, 0.25, -0.5] ]) 
+code = ["0", "1", "2"]
+my_model = pyvolve.Model("custom", {"matrix":matrix, "code":code} )
+
+my_partition = pyvolve.Partition(models = my_model, size = 1)
+
+my_evolver = pyvolve.Evolver(partitions = my_partition, tree = my_tree)
+my_evolver()
