bb591752626f9f1f3fb7f2cabd978d207884a436
sjspielman <stephanie.spielman@utexas.edu>
2015-06-14 15:16:02 -0500

    v0.4

--- setup.py
+++ setup.py
@@ -25,12 +25,12 @@ To install for a particular user (locally), enter -
 
 from setuptools import setup
 setup(name = 'Pyvolve', 
-    version = '0.4', 
+    version = 'v0.4', 
     description = 'Sequence simulation along phylogenies according to continuous-time Markov models',
     author = 'Stephanie J. Spielman', 
     author_email = 'stephanie.spielman@gmail.com', 
     url = 'https://github.com/sjspielman/pyvolve',
-    download_url = 'https://github.com/sjspielman/pyvolve/tarball/0.4',
+    download_url = 'https://github.com/sjspielman/pyvolve/tarball/v0.4',
     platforms = 'Tested on Mac OS X.',
     package_dir = {'pyvolve':'src'},
     packages = ['pyvolve', 'tests'],
